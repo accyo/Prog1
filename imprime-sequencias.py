b46d744c6976501b44f78239ea73f36ec4ba9ca1
Karen <karen.alves@ccc.ufcg.edu.br>
2020-11-25 10:58:34 -0300

    Update imprime-sequencias.py

--- imprime-sequencias.py
+++ imprime-sequencias.py
@@ -5,15 +5,18 @@ seq = 0
 
 while True: 
     
-    entrada = input().split()
+    entrada = input()
+    seq += 1
     if entrada != "fim":
+        entradas = entrada.split()
         sequencias = []
+    
         cont = 0
-        for i in range(len(entrada)): 
-            if int(entrada[i]) > alvo: 
+        for i in range(len(entradas)): 
+            if int(entradas[i]) > alvo: 
                 cont += 1
-            if cont > (len(entrada))//2: 
-                sequencias.append(int(entrada[i]))
-        
-                for _ in entrada: 
-                    print(_, end='')
+        if cont > (len(entradas))//2: 
+            print('sequencia{}: {}'.format(seq, entrada))
+    else:
+        break
+    
